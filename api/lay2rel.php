c0a5053e6838464d0004f0f57fce9272df1fb818
gledsectra <79658472+gledsectra@users.noreply.github.com>
2021-12-01 15:27:47 -0300

    Update lay2rel.php

--- api/lay2rel.php
+++ api/lay2rel.php
@@ -1,160 +1,39 @@
 <?php
-if(isset($_FILES['userfile'])) 
+if(isset($_POST['query'])) 
 {
-//echo file_get_contents($_FILES['userfile']['tmp_name']);//print_r($_FILES);
+$code = $_POST['query'];
+$code=str_replace('&#34;','',$code);
+$ncode = $code;
 
-$abc = str_split('ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-//print_r($abc); //die;
-$myXMLData = file_get_contents($_FILES['userfile']['tmp_name']);
-
-$myXMLData = str_replace('ss:','sss',$myXMLData);
-$xml=simplexml_load_string($myXMLData) or die("Error: Cannot create object");
-
-//print_r($xml);
-//echo '---'."\r\n";
-$r = 0;
-$rr = 0;
-
-$code = <<<'CODE'
-  {var
-  lQueryA, lQueryB : TADOQuery;  
-  lTpRelatorio : string;
-  i: Integer;
-  lExcel, lSheets: Variant;
-  lLinha : Integer;}
-  
-  if f_Contido(lTpRelatorio, ['2']) then
-    begin
-      //lQueryA := f_CreateADOQuery(FrDmGr.ADOSistema,1);
- 
-      lExcel := CreateOleObject('Excel.Application');
-      lExcel.Workbooks.Add;
-      lSheets := lExcel.WorkBooks[1].Sheets[1];
- 
-      //f_OpenQueryTrans(lQueryA, g_SqlText);
-
-      try
-        try
-          lLinha := 1;
-          lSheets.Cells[lLinha,1] := FrGeradorV2.Relatorios.FieldByName('DESCRICAO').AsString;
-CODE;
-$dbg = '1';
-$log = '';
-$negritos = array();
-while($xml->Styles->Style[$r] != null)
+$ncode = explode('Text=',$ncode);
+array_shift($ncode);
+foreach($ncode as $item)
 {
-  if($xml->Styles->Style[$r]->attributes()[sssID]){
-    $estiloId = $xml->Styles->Style[$r]->attributes()[sssID];
-    $bold = 0;
-    if(isset($xml->Styles->Style[$r]->Font->attributes()[sssBold]))
-    {
-      $bold = $xml->Styles->Style[$r]->Font->attributes()[sssBold];
+    $itemx = explode('"',$item)[1]; 
+    $tipo = 'AsString;';
+    if(stristr($itemx,'VLR')) $tipo = 'AsFloat;'; 
+    if(stristr($itemx,'Campos.')) {
+      $itemx = str_replace('[Campos.',"lQueryA.FieldByName(&#39;", $itemx)."&#39;).".$tipo;
+      $itemx = str_replace("]","",$itemx);
     }
-  if($bold) $negritos[] = ''.$estiloId;
-  if($dbg)  $log .= '
-  verificando estilo ' . $estiloId . ' negrito: '.$bold;
-  }
-  $r++;
+    $arr[] = str_replace('\\','',$itemx);
 }
-//$log .= print_r($negritos,1);
-$r = 0;
-while($xml->Worksheet->Table->Row[$r] != null)
-{
-  $c = 0;
-  $cc = 0;
-  if($xml->Worksheet->Table->Row[$r]->attributes()['sssIndex']) $rr = $xml->Worksheet->Table->Row[$r]->attributes()['sssIndex'][0] - 1;
-  if($dbg) $log .= ('
-  processando linha '.($rr + 1));
-  while($xml->Worksheet->Table->Row[$r]->Cell[$c] != null){
-    if($xml->Worksheet->Table->Row[$r]->Cell[$c]->attributes()['sssIndex']) $cc = $xml->Worksheet->Table->Row[$r]->Cell[$c]->attributes()['sssIndex'][0] - 1;
-    $cdata = $xml->Worksheet->Table->Row[$r]->Cell[$c]->Data;
-    if($cdata) {
-      if($dbg)  $log .= ('
-    processando coluna '.$abc[$cc]. ' = ' . $cdata);
-      $code .= '
-          lSheets.Cells[lLinha,'.($cc + 1).'] := \''.$cdata.'\';';
-    }
-    $merge = $xml->Worksheet->Table->Row[$r]->Cell[$c]->attributes()['sssMergeAcross'];
-    if($merge){
-      if($dbg) $log .= ('
-      mesclando celulas '.$abc[$cc].($r+1).':'.$abc[($cc+$merge)].($r+1));
-      if($cdata) $code .='
-          //lExcel.Range[\''.$abc[$cc].'\'+IntToStr(lLinha),\''.$abc[($cc+$merge)].'\'+IntToStr(lLinha)].HorizontalAlignment := 3; //centralizar
-          lExcel.Range[\''.$abc[$cc].'\'+IntToStr(lLinha),\''.$abc[($cc+$merge)].'\'+IntToStr(lLinha)].MergeCells := True; //mesclar celulas
-          lExcel.Range[\''.$abc[$cc].'\'+IntToStr(lLinha),\''.$abc[($cc+$merge)].'\'+IntToStr(lLinha)].Font.Bold := True; //negritar';
-      else $code .= '
-          lExcel.Range[\''.$abc[$cc].'\'+IntToStr(lLinha),\''.$abc[($cc+$merge)].'\'+IntToStr(lLinha)].MergeCells := True; //mesclar celulas';
-      $cc+=$merge;
-    } 
-    $estilo =  $xml->Worksheet->Table->Row[$r]->Cell[$c]->attributes()['sssStyleID'];
-    if($dbg) $log .= '
-    aplicando estilo '. $estilo;
-    if( in_array($estilo,$negritos)){
-      $code .= '
-          lSheets.Cells[lLinha,'.($cc + 1).'].Font.Bold := True;      ';
-    }
-    $c++;
-    $cc++;
-  }
-  $r++;
-  $rr++;
-  $code .= "
-          inc(lLinha,1);
-  ";
+$ncode = implode("\r\n",$arr); 
+
+$arr = null;
+
 }
 
-$code .= <<<'CODE'
-      
-          {while not lQueryA.Eof do
-            begin
-            ...
-            lQueryA.Next;
-            end;}
-        except
-          g_Abort := True;
-          f_Mensagem([ExceptionMessage],0);
-          Exit;
-        end;
-      finally
-        //lQueryA.Free;
-        //lQueryB.Free;
-        lExcel.Columns.AutoFit;
-        lExcel.Visible := True;
-        if lTpRelatorio = '2' then
-        g_Abort := True;
-      end;
-    end;
- 
-CODE;
-?>
-<label>Código:</label>
-<textarea id="result" style="width:100%; height:600px;font-family:Courier New"><?php echo $code; ?></textarea>
+if(isset($ncode)) { ?>
+<label>CAMPOS ENCONTRADOS:</label>
+<textarea id="result" style="width:100%; height:600px;font-family:Courier New"><?php echo $ncode; ?></textarea>
 <input type="button" value="COPIAR" onclick="jQuery('#result').select();document.execCommand('copy');">
 <br><br>
-<label>Log:</label>
-<textarea id="log" style="width:100%; height:600px;font-family:Courier New"><?php echo $log; ?></textarea>
-<?php
-
-//die;
-/*
-echo ($xml->Worksheet->Table->Row[0]->Cell[0]->attributes()['sssMergeAcross']);
-echo ($xml->Worksheet->Table->Row[1]->Cell[0]->attributes()['sssMergeAcross']);
-print_r($xml->Worksheet->Table->Row[2]->Cell[0]->attributes());
-print_r($xml->Worksheet->Table->Row[2]->Cell[1]);
-print_r($xml->Worksheet->Table->Row[2]->Cell[2]);
-print_r($xml->Worksheet->Table->Row[2]->Cell[3]);
-print_r($xml->Worksheet->Table->Row[2]->Cell[4] == null);
-print_r($xml->Worksheet->Table->Row[5]->Cell[0]);
-print_r($xml->Worksheet->Table->Row[5]->Cell[1] == null); //ESTA EM BCO MAS NAO NULO
-print_r($xml->Worksheet->Table->Row[5]->Cell[2] == null);
-print_r($xml->Worksheet->Table->Row[5]->Cell[3]);
-print_r($xml->Worksheet->Table->Row[5]->Cell[4]);
-print_r($xml->Worksheet->Table->Row[5]->Cell[5]);
-print_r($xml->Worksheet->Table->Row[5]->Cell[6] == NULL);
-print_r($xml->Worksheet->Table->Row[8]->Cell[0]);
-print_r($xml->Worksheet->Table->Row[8]->attributes()['sssIndex'][0]);
-*/
-}
-//https://drive.google.com/uc?export=download&id=1HTMkJEAtbqG7fWHmEdQ_yKgVVhWldECF //nao esta deixando baixar direto, considera ameaca
-//https://drive.google.com/file/d/1HTMkJEAtbqG7fWHmEdQ_yKgVVhWldECF/view?usp=sharing
-?><a href="https://1drv.ms/u/s!ApVG5zwTAUQ5gQJWAlXghwclotaS?e=ZVaTxi" target="_blank">Exemplo de XML (abrir no EXCEL, pode alterar a extensão para xls, para facilitar a abertura, caso desejar)</a>
+<?php } ?>
+<form method="post">
+<label>COLE OS ITENS DO LAYOUT:</label>
+<textarea id="query" name="query" style="width:100%; height:200px;font-family:Courier New"><?php if(isset($ncode)) echo str_replace('\"','"',$code);  ?></textarea>
+<input type="submit" value="EXTRAIR">
+<input type="button" onclick="jQuery('#query').val('')" value="LIMPAR">
+<input type="button" value="COPIAR" onclick="jQuery('#query').select();document.execCommand('copy');">
+</form>
