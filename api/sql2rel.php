0704e81ecc5672f10e78cc79d6c3efb442284e13
gledsectra <79658472+gledsectra@users.noreply.github.com>
2022-01-17 12:39:29 -0300

    Update sql2rel.php

--- api/sql2rel.php
+++ api/sql2rel.php
@@ -1,4 +1,5 @@
 <style>body{ font-family: arial }</style>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <?php
 if(isset($_POST['query'])) 
 {
